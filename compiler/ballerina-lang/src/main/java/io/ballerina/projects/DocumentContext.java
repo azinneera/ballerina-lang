fdcc03bc4e9f14b36437f034bad9f6a0cbdcb5a5
azinneera <azinneera@gmail.com>
2022-11-11 12:52:19 +0530

    Revert "Fix duplication of client modules for same url"
    This reverts commit 995829d384fe73686de06a526eec55ab1e264419.

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/DocumentContext.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/DocumentContext.java
@@ -325,18 +325,13 @@ class DocumentContext {
                             CompilerPlugins.annotationsAsStr(annotationsList))) {
                         continue;
                     }
+                    if (!CompilerPlugins.moduleExists(cachedPlugin.generatedModuleName(), currentPkg.project())) {
+                        // user has deleted the module
+                        return false;
+                    }
                     if (cachedPlugin.lastModifiedTime() != new File(cachedPlugin.filePath()).lastModified()) {
                         // the idl resource file has been modified
                         return false;
-                    }
-
-                    if (!CompilerPlugins.moduleExists(cachedPlugin.generatedModuleName(), currentPkg.project())) {
-                        if (idlPluginManager.generatedModuleConfigs().stream().noneMatch(moduleConfig ->
-                                moduleConfig.moduleDescriptor().name().moduleNamePart()
-                                        .equals(cachedPlugin.generatedModuleName()))) {
-                            // user has deleted the module
-                            return false;
-                        }
                     }
                     getGeneratedModuleEntry(cachedPlugin, lineRange);
                     return true;
@@ -444,9 +439,9 @@ class DocumentContext {
                 return localFilePath;
             }
 
-            String[] split = url.getFile().split("[~?=#@*+%{}<>/\\[\\]|\"^]");
+            String[] split = url.getFile().split("[~?=#@*+%{}<>\\[\\]|\"^]");
             fileName = split[split.length -1];
-            Path resourceName = Paths.get(fileName).getFileName();
+            Path resourceName = Paths.get(fileName);
             Path absResourcePath = this.currentPkg.project().sourceRoot().resolve(resourceName);
             Path resourcePath = this.currentPkg.project()
                     .documentPath(this.documentId).orElseThrow().relativize(absResourcePath);
